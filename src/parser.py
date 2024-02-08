b7d32b363518ab01b22ed2b11db87f7b6a25bd02
Lloyd Bush <lloydtbush@gmail.com>
2024-02-08 15:10:22 +0100

    add file reading and szenario directory

---
+++ src/parser.py
@@ -0,0 +1,38 @@
+import os
+
+# Tokens
+LPAREN      ="("
+RPAREN      =")"
+QUALIF      ="???"
+ADDPKT      ="+++"
+DIGITS      ="0123456789"
+
+# Parts
+TEXT    =""
+REQLEV  =0
+
+# read file
+def fread(file):
+    global REQLEV
+    file=open(file,"r")
+    buf=file.read()
+
+    for i,c in enumerate(buf):
+        if c in QUALIF:
+            if buf[i+1] in QUALIF and buf[i+2] in QUALIF:
+                s_num=""
+                for j,d in enumerate(buf,i):
+                    if d in DIGITS:
+                        s_num+=d
+                REQLEV=int(s_num)
+                
+    file.close()
+
+# Pfad zu Szenarien
+dir_path=os.path.dirname(os.path.abspath(__file__))+"/szenarien"
+
+for root,dirs,files in os.walk(dir_path):
+    for file in files:
+        if file.endswith(".txt"):
+            print(root+"/"+str(file))
+            fread("szenarien/"+file)
